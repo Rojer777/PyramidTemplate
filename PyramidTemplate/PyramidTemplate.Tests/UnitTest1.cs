f1703c933dba5f2f03fec00c829b6da714ef8b66
Rojer777 <rojer.roma@gmail.com>
2017-08-06 11:26:47 +0200

    Initial commit

---
+++ PyramidTemplate/PyramidTemplate.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PyramidTemplate.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
